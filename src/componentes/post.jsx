95a3d93665fdfd2b7691f780f74baebc9f54f6c4
WeFake123 <augustoagi12345@gmail.com>
2026-02-08 19:44:03 -0300

    cambios 2-8

--- src/componentes/post.jsx
+++ src/componentes/post.jsx
@@ -10,6 +10,7 @@ export const Post = ({ post, onClose }) => {
 
   const[msj, setMsj] = useState("");
   const[chats, setChats] = useState([])
+  const[reply, setReply] = useState("")
   
   useEffect(() => {
          fetch(`${API_URL}/inicio/${post.id}`)
@@ -18,14 +19,32 @@ export const Post = ({ post, onClose }) => {
              .catch(err => console.error(err));
      }, []);
 
+      const reversedChats = [];
 
-
-  console.log(chats)
+    for (let i = chats.length - 1; i >= 0; i--) {
+    reversedChats.push(chats[i]);
+    }
 
 
   const comentar = async (e) => {
 
   e.preventDefault();
+
+    let repeat = true;
+    let serialMsj;
+
+    while (repeat) {
+      serialMsj = Math.floor(Math.random() * 100000);
+
+      const existe = chats.some(i => i.serial === serialMsj);
+
+      if (!existe) {
+        repeat = false;
+      }
+    }
+
+
+    console.log(serialMsj);
 
       if(msj.length == 0){
         toast.error("Debes escribir algo");
@@ -36,10 +55,10 @@ export const Post = ({ post, onClose }) => {
     try {
     const response = await fetch(`${API_URL}/inicio/${post.id}`, {
       method: "POST",
-      body: JSON.stringify({ data: msj, idPost: post.id}),
+      body: JSON.stringify({ data: msj, idPost: post.id, serial: serialMsj, replyTo: reply}),
     headers: {
       "Content-Type": "application/json"
-    }
+    },
     });
 
     const data = await response.json();
@@ -51,6 +70,15 @@ export const Post = ({ post, onClose }) => {
   }
 }
 
+const goToChat = (serial) => {
+  const el = document.getElementById(`chat-${serial}`);
+  if (!el) return;
+
+  el.scrollIntoView({ behavior: "smooth", block: "center" });
+
+  el.classList.add("highlight");
+  setTimeout(() => el.classList.remove("highlight"), 1200);
+};
 
 
   return (
@@ -75,14 +103,20 @@ export const Post = ({ post, onClose }) => {
         </div>
 
         <div className="chat">
+                          {(reply) ? <div className="formChat-reply"><p className="formChat-reply-text" onClick={() => goToChat(reply)}>responderle a {reply}</p> <p className="formChat-reply-cancel" onClick={() => setReply("")}>cancelar</p></div> : null} 
+
             <form className="formChat-post" onSubmit={comentar}>
+
                 <input className="contenidosInput-post" type="text" placeholder="Comenta el post" value={msj} onChange={(e) => setMsj(e.target.value)}/>
                 <button className="botonEnviar-post">Enviar</button>    
             </form> 
 
-            <div>
-
-              {chats.map(({data,id}) => <p className="mensaje" key={id}>{data}</p>)}
+            <div className="chat-mensajes">
+              {reversedChats.map(({data,id, serial, replyTo}) => <div className="mensaje" key={id}  id={`chat-${serial}`}> 
+                                                  <p className="mensaje-serial" onClick={() => setReply(serial)}>{`>>${serial}`}</p>
+                                                  {replyTo ?  <p className="mensaje-reply" onClick={() => goToChat(replyTo)}>{`>>${replyTo}`}</p> : null}
+                                                  <p className="mensaje-texto">{data}</p>  
+                                                </div>)}
 
             </div>
 
