1dac6e8cf87d3b72af4349155820f85a70d3aebd
WeFake123 <augustoagi12345@gmail.com>
2026-02-26 02:42:29 -0300

    26/2 3

--- config.jsx
+++ config.jsx
@@ -1,4 +1,4 @@
-export const API_URL = "https://posted-9ped.onrender.com";
+//export const API_URL = "https://posted-9ped.onrender.com";
 
-//export const API_URL = "http://localhost:3000"
+export const API_URL = "http://localhost:3000"
 export const BD_URL = "https://api.cloudinary.com/v1_1/drgnllwbv/image/upload"
